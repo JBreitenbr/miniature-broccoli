1aead7d2dc1bb7a0888b8f89592b86405500243c
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-06-21 19:02:42 +0200

    Add tooltip functionality and update stacked bars

--- stacked2.js
+++ stacked2.js
@@ -39,3 +39,65 @@ d3.csv("https://raw.githubusercontent.com/JBreitenbr/Music-Genres/refs/heads/mai
   var color = d3.scaleOrdinal()
     .domain(subgroups)
     .range(['#ffd92f','#a6d854','#e78ac3',"#8da0cb"])
+
+    
+  //stack the data? --> stack per subgroup
+  var stackedData = d3.stack()
+    .keys(subgroups)
+    (data)
+
+
+
+  // ----------------
+  // Create a tooltip
+  // ----------------
+  var tooltip = d3.select("#my_dataviz")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    var subgroupName = d3.select(this.parentNode).datum().key;
+    var subgroupValue = d.data[subgroupName];
+    var artsy=d.data["group"];
+    tooltip
+        .html("Künstler: "+artsy+"<br>"+"Tageszeit: " +subgroupName + "<br>" + subgroupValue+" Minuten")
+        .style("opacity", 1)
+  }
+  var mousemove = function(d) {
+    tooltip
+      .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+      .style("top", (d3.mouse(this)[1]) + "px")
+  }
+  var mouseleave = function(d) {
+    tooltip
+      .style("opacity", 0)
+  }
+
+  // Show the bars
+  svg.append("g").attr("transform", "translate(330, 0) scale(-1, 1)")
+
+    .selectAll("g")
+    // Enter in the stack data = loop key per key = group per group
+    .data(stackedData)
+    .enter().append("g")
+      .attr("fill", function(d) { return color(d.key); })
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data(function(d) { return d; })
+      .enter().append("rect")
+        .attr("y", function(d) { return x(d.data.group); })
+        .attr("x", function(d) { return y(d[1]); })
+        .attr("width", function(d) { return y(d[0]) - y(d[1]); })
+        .attr("height",x.bandwidth())
+        .attr("stroke", "grey")
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave);
+  let keys=["vormittags","nachmittags","abends","nachts"];
